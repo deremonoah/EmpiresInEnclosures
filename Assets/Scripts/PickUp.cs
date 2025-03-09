da8e82ac4396f5575c2a2d24a91c42274c4b1388
Noah <deremo.noah@gmail.com>
2025-03-09 18:06:08 -0500

    item pickUp display working
    I realize I could probably use this for blue prints to its just what gets done at the end and for who
    
    could I use events?

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickUp : MonoBehaviour
 {
     public pickUp food;
     public float strength;
     public float TimeToPickUp;
-    [SerializeField] private float playerTimer;
-    [SerializeField] private float enemyTimer;
+    [SerializeField] private float progTimer;
+
     private bool playerHere, enemyHere;
     private UnitManager um;
+    public Image ProgressBar;
+    public Text progressText;
+
     private void Start()
     {
         um = FindObjectOfType<UnitManager>();
@@ -22,29 +26,27 @@ public class PickUp : MonoBehaviour
     {
         int team = other.gameObject.layer;
         //player's team
-        Debug.Log("in trigger");
         if (team == 7 && !enemyHere)
         {
-            playerTimer += Time.deltaTime;
+            progTimer += Time.deltaTime;
         }
         //enemies team
         else if (team == 6 && !playerHere)
         {
-            enemyTimer += Time.deltaTime;
+            progTimer -= Time.deltaTime;
         }
 
-        //who won the food
-        if (playerTimer > TimeToPickUp)
-        { PlayerGotFood(true); }
-        else if (enemyTimer > TimeToPickUp)
-        {
-            PlayerGotFood(false);
-        }
+        //when the prog timer is = to TimetoPickUp positive(player) or negative(enemy) 
+        if (Mathf.Abs(progTimer) >= TimeToPickUp)
+        { PlayerGotFood(); }
+
+        DisplayProgress();
     }
 
-    private void PlayerGotFood(bool isPlayer)
+    private void PlayerGotFood()
     {
-        if(isPlayer)
+        //if player has reached the time alternative is enemy reached negative version
+        if(progTimer>=TimeToPickUp)
         { 
             //player got it
             switch(food)
@@ -66,6 +68,17 @@ public class PickUp : MonoBehaviour
             }
         }
         Destroy(this.gameObject);
+    }
+
+    private void DisplayProgress()
+    {
+        //displays a bar that once is filled with your team's color you get the thing, does it start in the middle?
+        //would text be clearer?
+        //2 timers for different colors?
+        //progressText.text = ""+Mathf.Round(progTimer);
+
+        //starts at 0.5 and timer either gets closer to 1 for player or closer to zero
+        ProgressBar.fillAmount = (progTimer + TimeToPickUp) / (TimeToPickUp * 2);
     }
 }
 public enum pickUp { heal, PP, buffAttack, BuffDefense}
