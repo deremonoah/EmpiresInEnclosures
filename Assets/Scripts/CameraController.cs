1b887cbaaf99a7b44caed8fb542fd235592c1392
Flame <deremo.noah@gmail.com>
2025-12-24 14:10:41 -0600

    added zoom
    not sure our sprite translate as well when zoomed out but might have to change art in the future anyway

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float cameraSpeed;
-    public float sprintSpeed;
+    [Header("MoveSpeed variables")]
+    [SerializeField]
+    private float cameraSpeed;
+    [SerializeField]
+    private float sprintSpeed;
     private float currentSpeed;
+
+    [Header("Zoom variables")]
+    [SerializeField] private float minZoom;
+    [SerializeField] private float maxZoom;
+    [SerializeField] private float zoomSpeed;
+    private float currentZoom;
+    private Camera cam;
+
     void Start()
     {
-        
+        cam = GetComponent<Camera>();
     }
 
     void Update()
     {
+        //camera movement
         if(Input.GetKey(KeyCode.LeftShift))
         { currentSpeed = sprintSpeed; }
         else { currentSpeed = cameraSpeed; }
@@ -24,6 +36,19 @@ public class CameraController : MonoBehaviour
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
             transform.Translate(new Vector3( 0f, Input.GetAxisRaw("Vertical") * currentSpeed * Time.deltaTime, 0f));
+        }
+
+        //zoom
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        
+
+        if(scrollInput!=0)
+        {
+            Debug.Log(scrollInput);
+
+            float newsize = cam.orthographicSize - scrollInput * zoomSpeed;
+
+            cam.orthographicSize = Mathf.Clamp(newsize, minZoom, maxZoom);
         }
     }
 }
